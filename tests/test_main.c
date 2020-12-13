c2fb80af4fc17c237ec34f50a42536cc0cdfade2
Alex Kotov <kotovalexarian@gmail.com>
2020-12-13 20:03:03 +0500

    Test also new and delete

--- tests/test_main.c
+++ tests/test_main.c
@@ -9,10 +9,7 @@ static const char name[] = "/foobar";
 int main()
 {
     struct Shmemq consumer_shmemq;
-    struct Shmemq producer_shmemq;
-
     memset(&consumer_shmemq, 0, sizeof(consumer_shmemq));
-    memset(&producer_shmemq, 0, sizeof(producer_shmemq));
 
     assert(shmemq_init(&consumer_shmemq, name, true) == SHMEMQ_ERROR_NONE);
 
@@ -25,24 +22,28 @@ int main()
     assert(consumer_shmemq.buffer->header.read_frame_index == 0);
     assert(consumer_shmemq.buffer->header.write_frame_index == 0);
 
-    assert(shmemq_init(&producer_shmemq, name, false) == SHMEMQ_ERROR_NONE);
+    enum Shmemq_Error error;
+    struct Shmemq *producer_shmemq = shmemq_new(name, false, &error);
 
-    assert(strcmp(producer_shmemq.name, name) == 0);
-    assert(producer_shmemq.is_consumer == false);
-    assert(producer_shmemq.shm_id != -1);
-    assert(producer_shmemq.buffer != NULL);
-    assert(producer_shmemq.buffer->header.is_ready == false);
-    assert(producer_shmemq.buffer->header.frames_count == 0);
-    assert(producer_shmemq.buffer->header.read_frame_index == 0);
-    assert(producer_shmemq.buffer->header.write_frame_index == 0);
+    assert(producer_shmemq != NULL);
+    assert(error == SHMEMQ_ERROR_NONE);
 
-    assert(consumer_shmemq.buffer != producer_shmemq.buffer);
+    assert(strcmp(producer_shmemq->name, name) == 0);
+    assert(producer_shmemq->is_consumer == false);
+    assert(producer_shmemq->shm_id != -1);
+    assert(producer_shmemq->buffer != NULL);
+    assert(producer_shmemq->buffer->header.is_ready == false);
+    assert(producer_shmemq->buffer->header.frames_count == 0);
+    assert(producer_shmemq->buffer->header.read_frame_index == 0);
+    assert(producer_shmemq->buffer->header.write_frame_index == 0);
+
+    assert(consumer_shmemq.buffer != producer_shmemq->buffer);
 
     consumer_shmemq.buffer->header.is_ready = true;
-    assert(producer_shmemq.buffer->header.is_ready == true);
+    assert(producer_shmemq->buffer->header.is_ready == true);
 
     assert(shmemq_finish(&consumer_shmemq) == SHMEMQ_ERROR_NONE);
-    assert(shmemq_finish(&producer_shmemq) == SHMEMQ_ERROR_NONE);
+    assert(shmemq_delete(producer_shmemq) == SHMEMQ_ERROR_NONE);
 
     return 0;
 }
